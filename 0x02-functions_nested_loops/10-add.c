fa89c0453254b7db34993407cc53a3a924ee6a27
Patrick <patricknwosu360@gmail.com>
2023-10-12 01:37:54 +0100

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - adds two intgerr
+ *
+ * @n: first integer
+ *
+ * @b: second integer
+ *
+ *Return: return 0 if program is successful
+ */
+int add(int n, int b)
+{
+	int result = n + b;
+
+	return (result);
+}
