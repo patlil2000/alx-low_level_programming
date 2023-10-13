e99a83b3ec9cfcc52a13a8d8b656fd44801e8eed
Patrick <patricknwosu360@gmail.com>
2023-10-13 23:45:39 +0100

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - prints FizzBuzz,Fizz,and Buzz
+ *
+ * Return: return 0 if program is successful
+ */
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+	}
+	printf("\n");
+}
