eda24571704d2a0977f8ca29d3bdced4fde1f0ae
Patrick <patricknwosu360@gmail.com>
2023-10-31 19:37:31 +0100

    new

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -9,6 +9,7 @@ int _atoi(char *s)
 {
 	int i = 1;
 	unsigned int num = 0;
+
 	do {
 		if (*s == '-')
 			i *= -1;
@@ -17,5 +18,5 @@ int _atoi(char *s)
 		else if (num > 0)
 			break;
 	} while (*s++);
-	return (num *i);
-
+	return (num * i);
+}
--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -15,17 +15,16 @@ char *_strstr(char *haystack, char *needle)
 
 	if (*needle == 0)
 		return (haystack);
-	
+
 	while (*haystack)
 	{
 		i = 0;
 
 		if (haystack[i] == needle[i])
 		{
-			do{
+			do {
 				if (needle[i + 1] == '\0')
 					return (haystack);
-
 				i++;
 			} while (haystack[i] == needle[i]);
 		}
