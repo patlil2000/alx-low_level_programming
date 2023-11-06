c46ae3bfbfb0e700fdccf6cacdc0b802a0ac6541
Patrick <patricknwosu360@gmail.com>
2023-11-06 22:39:58 +0100

    new

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - describes a dog
+ *@age: First member
+ *@name: Second member
+ *@owner: Third member
+ *
+ * Description: This is a structure that describes
+ * a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H*/
