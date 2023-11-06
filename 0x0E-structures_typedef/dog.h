6e0207512b6efe509934dac0c9a8673aad235597
Patrick <patricknwosu360@gmail.com>
2023-11-07 00:54:12 +0100

    new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,10 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ *dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 #endif /* DOG_H*/
