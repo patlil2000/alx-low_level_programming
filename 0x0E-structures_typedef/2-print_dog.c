4a72773aa7b14257dbcc1f39d4ea177ceb9169c7
Patrick <patricknwosu360@gmail.com>
2023-11-08 01:26:47 +0100

    new

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,35 +2,28 @@
 #include <stdio.h>
 #include <stddef.h>
 /**
- *
- *
+ *print_dog - prints a struct dog
+ *@d: dog identification
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
 	if ((*d).name == NULL)
-	{
-		printf("Name: (nil)\n");
-	}
+		printf("nil\n");
 	else
-	{
-		printf("Name: %s\n", (*d).name);
-	}
-	if((*d).age == NULL)
-	{
-		printf("Age: (nil)\n");
-	}
+		printf("Name: %s\n", d->name);
+
+	if ((*d).age < 0)
+		printf("nil\n");
 	else
-	{
-		printf("Age: %f\n", (*d).age);
-	}
-	if((*d).owner == NULL)
-	{
-		printf("Owner: (nil)\n");
-	}
+		printf("Age: %f\n", d->age);
+
+	if ((*d).owner == NULL)
+		printf("nil\n");
 	else
-	{
-		printf("Owner: %s\n", (*d).owner);
-	}
-	if((*d) = NULL)
+		printf("Owner: %s\n", d->owner);
+
 
 }
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,8 @@
+#include "dog.h"
+/**
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
