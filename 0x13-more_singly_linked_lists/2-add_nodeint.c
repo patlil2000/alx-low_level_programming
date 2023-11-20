a424989f6bfa3261604fbf448481d4fd97f3a5ac
Patrick <patricknwosu360@gmail.com>
2023-11-20 15:48:05 +0100

    functions that inserts a node to the beginning of a linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *add_nodeint - adds a new node at the beginning of a listint_t list.
+ *@n: the integer portion of the node
+ *@head: the head of the list
+ *Return: the address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = (listint_t *)malloc(sizeof(listint_t));
+	if (temp == NULL)
+	{
+		printf("Memory Allocation failed");
+	}
+	temp->next = *head;
+	temp->n = n;
+	*head = temp;
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	return (*head);
+
+}
